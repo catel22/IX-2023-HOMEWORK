b90c9c9b13ba6bd6dfb93cf5285936eb3eee37c4
hayleypeterson <112511498+hayleypeterson@users.noreply.github.com>
2023-05-31 12:56:40 +0200

    added my function

--- week_1/day_3/main.js
+++ week_1/day_3/main.js
@@ -11,3 +11,16 @@ function fibSequence() {
 }
 
 fibSequence();
+
+//hayley's function
+
+function fibonacci() {
+    let last = 0;
+    let cur = 1;
+    for (let i =0; i<10; i++) {
+        console.log(cur);
+        temp = cur;
+        cur = cur+last;
+        last = temp;
+    }
+}
