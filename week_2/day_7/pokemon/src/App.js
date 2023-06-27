a5d6a20d751096af4fcc9204f5b0c1b06688c1d9
Cate Lewison <clewison@hmc.edu>
2023-06-27 15:42:49 +0200

    fixing API fetch

--- week_2/day_7/pokemon/src/App.js
+++ week_2/day_7/pokemon/src/App.js
@@ -1,51 +1,52 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import PokemonTable from './components/PokemonTable';
-import PokemonForm from './components/PokemonForm';
-import { Pokemon } from './models/pokemon';
+import "bootstrap/dist/css/bootstrap.min.css";
+import PokemonTable from "./components/PokemonTable";
+import PokemonForm from "./components/PokemonForm";
+import { Pokemon } from "./models/pokemon";
 
 function App() {
-  const [pokemon, setPokemon] = useState([]);
+  const [pokemon, setPokemon] = useState(null);
 
   // Http adress
-  const url = 'https://pokeapi.co/api/v2/pokemon/';
+  const url = "https://pokeapi.co/api/v2/pokemon/";
 
   // Function to fetch the pokemon
   async function fetchPokemon(pokemonName) {
+    pokemonName = pokemonName.toLowerCase();
     const res = await fetch(url + pokemonName, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-  });
+    });
 
-  const data = await res.json();
-  console.log(data);
+    const data = await res.json();
+    console.log(data);
 
-  let name = data.species.name;
+    let name = data.species.name;
 
-  //get moves that can be learned
-  let moves = [];
-  for(let i=0; i<data.moves.length; i++) {
+    //get moves that can be learned
+    let moves = [];
+    for (let i = 0; i < data.moves.length; i++) {
       moves.push(data.moves[i].move.name);
+    }
+
+    //get img
+    const imageURL = data.sprites.front_default;
+
+    //get base stats (hp, attack, defense, spatk, spdef, spd )
+    let baseStats = [];
+    for (let i = 0; i < 6; i++) {
+      baseStats.push(data.stats[i].base_stat);
+    }
+
+    console.log(baseStats);
+    let pokemon = new Pokemon(name, moves, imageURL, baseStats);
+    console.log(pokemon);
+    setPokemon(pokemon);
   }
-
-  //get img
-  const imageURL = data.sprites.front_default;
-
-  //get base stats (hp, attack, defense, spatk, spdef, spd )
-  let stats = [];
-  for (let i=0; i<6; i++) {
-  stats.push(data.stats[i].base_stat);
-  }
-
-  let pokemon = new Pokemon(name, moves, imageURL, stats);
-  console.log(pokemon);
-  setPokemon(pokemon);
-  }
-
 
   return (
     <div className="text-center mt-5 mx-5">
--- week_2/day_7/pokemon/src/components/PokemonTable.js
+++ week_2/day_7/pokemon/src/components/PokemonTable.js
@@ -1,59 +1,111 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function PokemonTable(props) {
   if (!props.pokemon) {
     return;
   }
-  
+
   return (
     <div className="card mx-5 mt-3 p-4 bg-light">
-        <h3> {props.pokemon.name} </h3>
+      <h3>
+        {" "}
+        {props.pokemon.name.charAt(0).toUpperCase() +
+          props.pokemon.name.slice(1)}{" "}
+      </h3>
 
-        <div>
-        {props.pokemon.image && <img src={props.pokemon.image} alt="Pokemon" />}
+      <div>
+        {props.pokemon.imageURL && (
+          <img src={props.pokemon.imageURL} alt="Pokemon" />
+        )}
+      </div>
+
+      {/* get base stats (hp, attack, defense, spatk, spdef, spd ) */}
+      <h6 className="mt-1"> Base stats: </h6>
+
+      <div className="px-5 mx-5" style={{ overflow: "auto" }}>
+        <div style={{ float: "left", width: "50%" }}>
+          <p>
+            {" "}
+            <strong> HP: </strong> {props.pokemon.stats[0]}
+          </p>
+          <p>
+            {" "}
+            <strong> Atk: </strong> {props.pokemon.stats[1]}
+          </p>
+          <p>
+            {" "}
+            <strong> Def: </strong> {props.pokemon.stats[2]}
+          </p>
         </div>
-
-        {/* get base stats (hp, attack, defense, spatk, spdef, spd ) */}
-        <h6 className = "mt-1"> Base stats: </h6>
-
-
-    <div className = "px-5 mx-5" style={{ overflow: 'auto' }}>
-        <div  style={{ float: 'left', width: '50%' }}>
-            <p> <strong> HP: </strong> {props.pokemon.baseStats}</p> 
+        <div style={{ float: "left", width: "50%" }}>
+          <p>
+            {" "}
+            <strong> SpAtk: </strong> {props.pokemon.stats[3]}
+          </p>
+          <p>
+            {" "}
+            <strong> SpDef: </strong> {props.pokemon.stats[4]}
+          </p>
+          <p>
+            {" "}
+            <strong> Speed: </strong> {props.pokemon.stats[5]}
+          </p>
         </div>
-         <div  style={{ float: 'left', width: '50%' }}>
-            <p> <strong> SpAtk: </strong> {props.pokemon.baseStats}</p> 
-        </div>
-    </div>        
+      </div>
 
-        <div id="accordion">
-      <div className="card">
-        <div className="card-header" id="headingOne">
-          <h5 className="mb-0">
-            <button
-              className="btn btn-link"
-              data-toggle="collapse"
-              data-target="#collapseOne"
-              aria-expanded="true"
-              aria-controls="collapseOne"
-              style={{ background: 'none', border: 'none', textDecoration: 'none', color: 'black'}}
-            >
-              Moves that {props.pokemon.name} can learn (click here to collapse):
-            </button>
-          </h5>
+      <div id="accordion">
+        <div className="card">
+          <div className="card-header" id="headingOne">
+            <h5 className="mb-0">
+              <button
+                className="btn btn-link"
+                data-toggle="collapse"
+                data-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+                style={{
+                  background: "none",
+                  border: "none",
+                  textDecoration: "none",
+                  color: "black",
+                }}
+              >
+                Moves that{" "}
+                {props.pokemon.name.charAt(0).toUpperCase() +
+                  props.pokemon.name.slice(1)}{" "}
+                can learn:
+              </button>
+            </h5>
+          </div>
+
+          <div
+            id="collapseOne"
+            className="collapse show"
+            aria-labelledby="headingOne"
+            data-parent="#accordion"
+          >
+            <div className="card-body">
+              <div className="row">
+                <div className="col">
+                  {props.pokemon.moves
+                    .slice(0, Math.ceil(props.pokemon.moves.length / 2))
+                    .map((element, index) => (
+                      <p key={index}>{element}</p>
+                    ))}
+                </div>
+                <div className="col">
+                  {props.pokemon.moves
+                    .slice(Math.ceil(props.pokemon.moves.length / 2))
+                    .map((element, index) => (
+                      <p key={index}>{element}</p>
+                    ))}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-
-
-
-
-    </div>
-        
   );
-
-  
-
-
 }
